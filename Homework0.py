853072989a7b72ad6069aa5a211dc00cb62a029a
Arissa <ajb321@pitt.edu>
2025-08-28 17:39:11 -0400

    image load, image show, crop image working

---
+++ Homework0.py
@@ -0,0 +1,18 @@
+import cv2
+
+import os 
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+#Load the image in grayscale
+apollo_image = cv2.imread('Data/Apollo_11_Launch.jpg', cv2.IMREAD_GRAYSCALE)
+
+#View Image
+plt.imshow(apollo_image,cmap="gray")
+plt.show()
+
+#crop the image 
+cropped_image = apollo_image[200:600, 200:900]
+plt.imshow(cropped_image,cmap="gray")
+plt.show()
